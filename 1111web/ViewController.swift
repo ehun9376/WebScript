b2568dc07b09672f995f3283cc98b3b87fb40547
陳逸煌 <joh1234567892001@gmail.com>
2024-08-07 14:05:58 +0800

    Initial Commit

---
+++ 1111web/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1111web
+//
+//  Created by 陳逸煌 on 2024/8/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 1111webUITests/_111webUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _111webUITestsLaunchTests.swift
+//  1111webUITests
+//
+//  Created by 陳逸煌 on 2024/8/7.
+//
+
+import XCTest
+
+final class _111webUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
